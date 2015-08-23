1e4a741233bcb425d3360857baf25074dbc3c0bb
Lorenzo Delana <lorenzo.delana@gmail.com>
2015-08-23 16:49:51 +0200

    added default nuspec to use with CreateNewNuGetPackageFromProjectAfterEachBuild tool. added lib0-net45 changelog.

---
+++ src/Lib0-net45/ChangeLog.md
@@ -0,0 +1,3 @@
+﻿# ChangeLog
+## 1.0.2
+- Added nuspec file.
--- src/Lib0-net45/Properties/AssemblyInfo.cs
+++ src/Lib0-net45/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Lib0-nf")]
+[assembly: AssemblyTitle("Lib0-net45")]
 [assembly: AssemblyDescription("A library for daily use.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lib0-nf")]
+[assembly: AssemblyCompany("Lorenzo Delana")]
+[assembly: AssemblyProduct("Lib0-net45")]
 [assembly: AssemblyCopyright("Copyright © Lorenzo Delana 2015")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("xxx")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
